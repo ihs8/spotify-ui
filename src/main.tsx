2ac51d3a856cf5d5413c9c5e5ba828016ec02785
Iheb Blhj <this.iheb@gmail.com>
2023-12-03 21:46:02 +0100

    fix homepage uri

--- src/main.tsx
+++ src/main.tsx
@@ -2,16 +2,18 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 import { QueryClient, QueryClientProvider } from 'react-query'
+import { publicUri } from './credentials.tsx'
 const queryClient = new QueryClient();
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={publicUri}>
     <QueryClientProvider client={queryClient}>
-    <Routes>
+    {/* <Routes>
     <Route path='/spotify-ui/*'  element= {<App/>} />
-    </Routes>
+    </Routes> */}
+    <App/>
     </QueryClientProvider>
     </BrowserRouter>
   </React.StrictMode>,
