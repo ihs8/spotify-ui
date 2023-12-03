e7d0642b920073800c2b804d75fac7e9fadbd325
Iheb Blhj <this.iheb@gmail.com>
2023-12-03 21:52:31 +0100

    fix local version

--- src/main.tsx
+++ src/main.tsx
@@ -2,18 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import { QueryClient, QueryClientProvider } from 'react-query'
-import { publicUri } from './credentials.tsx'
 const queryClient = new QueryClient();
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter basename={publicUri}>
+    <BrowserRouter >
     <QueryClientProvider client={queryClient}>
-    {/* <Routes>
+    <Routes>
     <Route path='/spotify-ui/*'  element= {<App/>} />
-    </Routes> */}
-    <App/>
+    </Routes>
     </QueryClientProvider>
     </BrowserRouter>
   </React.StrictMode>,
